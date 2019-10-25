e04621457fe65cbc52c954a46433df5955ffddd3
markjapp <mapp@vdabtec.com>
2019-10-25 13:17:11 -0400

    Update routines

---
+++ MapSupport/maps_usgs.js
@@ -0,0 +1,58 @@
+function buildIcon_USGS(buoy) {
+var icon;
+var HUSub;
+
+const VERYHIGH = 97.0,
+ HIGH = 92.0,
+ MEDIUMHIGH = 90.0,
+ MEDIUM = 50,
+ LOW = 0;
+ code = buoy.HUCode.substring(0, 2);
+HUSub = parseFloat(code);
+
+// Get first part of name by checking the HUSub.
+var namePart1;
+var namePart2;
+
+if (HUSub == uniquecodes[0]){
+namePart1 = "huA";
+    }
+else if (HUSub == uniquecodes[1]){
+namePart1 = "huB";  
+}
+else if (HUSub == uniquecodes[2]){
+namePart1 = "huC";  
+}
+else if (HUSub == uniquecodes[3]){
+namePart1 = "huD";
+}
+
+// Get second part of name by checking the percent
+
+if (buoy.Percentile >= VERYHIGH){
+namePart2 = "veryhighpcnt"; 
+}
+else if (buoy.Percentile >= HIGH){
+namePart2 = "highpcnt"; 
+}
+else if (buoy.Percentile >= MEDIUMHIGH){
+namePart2 = "mediumhighpcnt"; 
+}
+else if (buoy.Percentile >= MEDIUM){
+namePart2 = "mediumpcnt"; 
+}
+else if (buoy.Percentile >= LOW){
+namePart2 = "lowpcnt"; 
+}
+
+
+icon = {
+url: 'http://gldw.org/docs/icons/'+namePart1+'_'+namePart2+'.png',
+scaledSize: new google.maps.Size(20, 20), // scaled size
+    origin: new google.maps.Point(0,0), // origin
+        anchor: new google.maps.Point(10,10) // anchor
+}
+
+
+return icon;
+}
--- MapSupport/mapsupport.js
+++ MapSupport/mapsupport.js
@@ -255,16 +255,29 @@ function buildContent_NDBC(buoy) {
 				contentString += getHTMLFormattedAlertingMetric(obj2, buoy[obj2], (buoy[obj2] > 9));
 				break;
 
+			case "Link":
+				break;
+
 			default:
 				contentString += getHTMLFormattedMetric(obj2, buoy[obj2]);
 				break;
 		}
 
 	}
+	for (var obj2 in buoy) {
+		switch (obj2) {
 
-	contentString += "<br/> <b> <center> <a href='https://www.ndbc.noaa.gov' >"
-	contentString += "<img src='https://gldw.org/docs/icons/ndbc.png' alt='Link to NDBC' width='75' height='35' >";
-	contentString += "</a> </center> </b>"
+			case "Link":
+				var url = buoy[obj2];
+				contentString += "<br/> <b> <center> <a href=" + url + " >"
+				contentString += "<img src='https://gldw.org/docs/icons/ndbc.png' alt='Link to NDBC' width='75' height='35' >";
+				contentString += "</a> </center> </b>"
+				break;
+
+			default:
+				break;
+		}
+	}
 	contentString += "</div>";
 	return contentString;
 }
